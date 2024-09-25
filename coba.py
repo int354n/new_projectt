759c5b6e87253ed3130035e6e4e9466aaca9591d
intan fauziah <fauziahintan640@gmail.com>
2024-09-26 02:06:11 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -1,2 +1,59 @@
 name="intan fauziah"
 print (name)
+
+x=3
+y=4
+print(x+y)
+
+num="2,0"
+print(num,'is type data',type(num))
+
+language=["swift","java","python"]
+
+print(language[-1])
+
+product = ("microssft","xbox",49.00)
+
+print (type(product[2]))
+ 
+name='python'
+
+print(name)
+
+capital_city={'nepal': 'india' ,'italy' : 'paris'}
+print(capital_city)
+
+y1={3:'a'}
+y2=3
+y4='hello'
+y5='hello'
+
+print('3' not in y5)
+
+agelist=[1,2,3,4]
+print(agelist)
+
+
+my_list=['q','w','d','e','r','t']
+print(my_list[2:3])
+
+thislist=['apel','manggis','jeruk','mangga']
+thislist[1:2]='blackcurrent'
+
+print(thislist)
+
+number=[1,2,3,4]
+
+#number.append(7)
+
+#event_number=[11,12,13,15]
+#number.extend(event_number)
+
+number.insert(12,13) #add element second position
+print(number)
+
+
+
+#del(bahasa[1]) del for int
+#bahasa.remove('R') # remove for string
+#print(bahasa)
